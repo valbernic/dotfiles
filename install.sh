144da00e327ba2462049a4c3e3821cc70459f0ba
Valentin Lafage <vlafage.dev@free.fr>
2025-02-03 15:38:43 +0100

    add symlink output
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>
    (cherry picked from commit ff475b39cd0671ada32a2a122eae27691597843e)

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash -e
 
-ln -sf $PWD/vim/.vimrc $HOME
-ln -sf $PWD/tmux/.tmux.conf $HOME
-ln -sf $PWD/git/.gitconfig $HOME
 
-ln -sf $PWD/bash/.bashrc $HOME
-ln -sf $PWD/bash/.bash_aliases $HOME
-ln -sf $PWD/bash/.bash_variables $HOME
-source $HOME/.bashrc
+create_symlink() {
+  local src=$1
+  local dst=${2:-$HOME}
 
-ln -sf $PWD/ssh/config $HOME/.ssh
-ln -sf $PWD/ssh/agent.toml $HOME/.config/1Password/ssh
+  ln -sf "$src" "$dst"
+  echo "Symlink created: $dst -> $src"
+}
+
+create_symlink "$PWD/git/.gitconfig"
+create_symlink "$PWD/tmux/.tmux.conf"
+create_symlink "$PWD/vim/.vimrc"
+
+create_symlink "$PWD/bash/.bashrc"
+create_symlink "$PWD/bash/.bash_aliases"
+create_symlink "$PWD/bash/.bash_variables"
+source "$HOME/.bashrc"
+
+create_symlink "$PWD/ssh/config" "$HOME/.ssh"
+create_symlink "$PWD/ssh/agent.toml" "$HOME/.config/1Password/ssh"
 
