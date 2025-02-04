75a7835c5c4e02e599ecb71743cd7d5660b48aab
Valentin Lafage <vlafage.dev@free.fr>
2025-02-04 14:32:06 +0100

    rework installation script
    install bash files before anything else
    
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>
    (cherry picked from commit 8f6e22c9b6ff9950ec2b576ef9a37927c99e1d4b)

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ create_symlink() {
   echo "Symlink created: $dst -> $src"
 }
 
+set_up_bash() {
+  create_symlink "$PWD/bash/.bashrc"
+  create_symlink "$PWD/bash/.bash_variables"
+  source "$HOME/.bashrc"
+}
+
 set_up_git() {
   local src="$PWD/git/.gitconfig"
   local dst="/mnt/c/Users/Shadow"
@@ -19,16 +25,17 @@ set_up_git() {
   echo "$src has been copied to $dst" 
 }
 
-create_symlink "$PWD/.dircolors"
-
+# bash
+set_up_bash
+# git
 set_up_git
-
+# ssh
+create_symlink "$PWD/ssh/config" "$HOME/.ssh"
+# tmux
 create_symlink "$PWD/tmux/.tmux.conf"
+# vim
 create_symlink "$PWD/vim/.vimrc"
 
-create_symlink "$PWD/bash/.bashrc"
-create_symlink "$PWD/bash/.bash_variables"
-source "$HOME/.bashrc"
-
-create_symlink "$PWD/ssh/config" "$HOME/.ssh"
+# misc
+create_symlink "$PWD/.dircolors"
 
