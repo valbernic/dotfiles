74218177a732fa4932af30b0e874a556008ed57f
Valentin Lafage <vlafage.dev@free.fr>
2025-02-04 14:32:06 +0100

    rework installation script
    install bash files before anything else
    
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>
    (cherry picked from commit 8f6e22c9b6ff9950ec2b576ef9a37927c99e1d4b)

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-
 create_symlink() {
   local src=$1
   local dst=${2:-$HOME}
@@ -9,15 +8,32 @@ create_symlink() {
   echo "Symlink created: $dst -> $src"
 }
 
+set_up_bash() {
+  create_symlink "$PWD/bash/.bashrc"
+  create_symlink "$PWD/bash/.bash_aliases"
+  create_symlink "$PWD/bash/.bash_variables"
+  source "$HOME/.bashrc"
+}
+
+set_up_ssh_agent() {
+  local dst="$HOME/.config/1Password/ssh"
+  mkdir -p "$dst"
+  create_symlink "$PWD/ssh/agent.toml" "$dst"
+}
+
+set_up_ssh() {
+  create_symlink "$PWD/ssh/config" "$HOME/.ssh"
+  set_up_ssh_agent
+}
+
+set_up_bash
+
+# git
 create_symlink "$PWD/git/.gitconfig"
+# ssh
+set_up_ssh
+# tmux
 create_symlink "$PWD/tmux/.tmux.conf"
+# vim
 create_symlink "$PWD/vim/.vimrc"
-
-create_symlink "$PWD/bash/.bashrc"
-create_symlink "$PWD/bash/.bash_aliases"
-create_symlink "$PWD/bash/.bash_variables"
-source "$HOME/.bashrc"
-
-create_symlink "$PWD/ssh/config" "$HOME/.ssh"
-create_symlink "$PWD/ssh/agent.toml" "$HOME/.config/1Password/ssh"
 
