343b3ad77163651fda4a4cd2d08c1900bd83c274
Valentin Lafage <valentin.lafage@celadodc-rswl.com>
2025-02-17 17:14:39 +0100

    set up bash
    Signed-off-by: Valentin Lafage <valentin.lafage@celadodc-rswl.com>

--- install.sh
+++ install.sh
@@ -1,2 +1,24 @@
 #!/bin/bash -e
 
+create_symlink() {
+  local src=$1
+  local dst=${2:-$HOME}
+
+  dst="${dst%/}"
+
+  if [ -d "$dst" ]; then
+    dst="$dst/$(basename "$src")"
+  fi
+
+  ln -sf "$src" "$dst"
+  echo "Symlink created: $dst -> $src"
+}
+
+set_up_bash() {
+  create_symlink "$PWD/bash/.bashrc"
+  create_symlink "$PWD/bash/.bash_variables"
+  source "$HOME/.bashrc"
+}
+
+set_up_bash
+
